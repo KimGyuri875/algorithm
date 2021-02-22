be1dcb4c0c8ee4457a85df96a8b63bb0f6dd8f01
kinggyu <63337662+KimGyuri875@users.noreply.github.com>
2021-02-22 10:29:18 +0900

    Update 3-6 1이 될 때까지.py

--- 이것이취업을위한코딩테스트다/ch03_그리디/3-6 1이 될 때까지.py
+++ 이것이취업을위한코딩테스트다/ch03_그리디/3-6 1이 될 때까지.py
@@ -4,10 +4,14 @@
 n, k = map(int, input().split())
 result = 0
 while True :
-  # 1씩 빼기
+  # 나눠서 나머지가 없는 값으로 target을 만든다. 
+  # n // k를 통해서 몫을 구하고 k를 곱하면 나눠어 떨어지는 값이 된다
+  # target은 그냥 temp 변수
   target = (n//k) * k
-  result += (n-result)
+  # 나머지값을 result에 넣기
+  result += (n-target)
   n = target
+  # n이 k보다 작을 때(즉, 이상 나눌 수 없을 때) 반복문 탈출
   if n < k:
     break
   result += 1
