bdba82814a0ffedd1b998ad22bfaa3f6fa82ef79
kinggyu <63337662+KimGyuri875@users.noreply.github.com>
2021-04-01 15:55:23 +0900

    Update 7576.py

--- baekjoon/깊이,너비 우선 탐색(DFS,BFS)/7576.py
+++ baekjoon/깊이,너비 우선 탐색(DFS,BFS)/7576.py
@@ -37,3 +37,36 @@ if flag:
     print('-1')
 else:
     print(max(map(max, tomato))-1)                                # -1 을 해준 이유는 day1과 이미익은 토마토인 '1'과차이를 주기 위해서 day2부터 계
+
+    
+# 토마토
+# 안익은 토마토의 인접 토마토가 익었으면 익는다
+# 최소 일수
+# 더 깔끔한 
+import sys
+from collections import deque
+input = sys.stdin.readline
+m, n = map(int, input().split())
+t = [list(map(int, input().split())) for _ in range(n)]
+deq = deque()
+mx = [-1,1,0,0]
+my = [0,0,-1,1]
+for i in range(n):
+    for j in range(m):
+        if t[i][j] == 1:
+            deq.append([i, j, 0])
+
+while deq:
+    x, y, time = deq.pop()
+    for i, j in zip(mx, my):
+        if 0<=i+x<n and 0<=j+y<m:
+            if t[i+x][j+y] == 0:
+                t[i+x][j+y] = 1
+                deq.appendleft([i+x, j+y, time+1])
+
+for i in t:
+    for j in i:
+        if j == 0:
+            print('-1')
+            sys.exit()
+print(time)
